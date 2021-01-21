394ef6fef9d2eda9bb9fd182f533d65839ad857d
BuildTools <unconfigured@null.spigotmc.org>
2021-01-21 10:30:54 -0800

    Improved the voxelshapes of the balloon block, balloon string, and the empty vase. Also updated the balloon block (the item was not updated) model.

--- src/main/java/me/pumpkinbear111/valentinesday/blocks/balloon_string.java
+++ src/main/java/me/pumpkinbear111/valentinesday/blocks/balloon_string.java
@@ -2,28 +2,33 @@ package me.pumpkinbear111.valentinesday.blocks;
 
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
-import net.minecraft.block.HorizontalBlock;
 import net.minecraft.block.SoundType;
 import net.minecraft.block.material.Material;
-import net.minecraft.state.DirectionProperty;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.shapes.IBooleanFunction;
 import net.minecraft.util.math.shapes.ISelectionContext;
 import net.minecraft.util.math.shapes.VoxelShape;
 import net.minecraft.util.math.shapes.VoxelShapes;
 import net.minecraft.world.IBlockReader;
-
 import java.util.stream.Stream;
 
 public class balloon_string extends Block {
 
-    public static final DirectionProperty FACING = HorizontalBlock.HORIZONTAL_FACING;
-
     public balloon_string() {
         super(Block.Properties.create(Material.WOOL)
-                .hardnessAndResistance(.8f, .8f)
                 .sound(SoundType.CLOTH)
+                .setAir()
+                .zeroHardnessAndResistance()
                 .notSolid());
     }
+
+    @Override
+    public VoxelShape getShape(BlockState state, IBlockReader worldIn, BlockPos pos, ISelectionContext context) {
+        return SHAPE_N;
+    }
+
+    private static final VoxelShape SHAPE_N = Stream.of(
+            Block.makeCuboidShape(7.5, 0, 7.5, 8.5, 16, 8.5)
+    ).reduce((v1, v2) -> {return VoxelShapes.combineAndSimplify(v1, v2, IBooleanFunction.OR);}).get();
 
 }
--- src/main/java/me/pumpkinbear111/valentinesday/blocks/empty_vase.java
+++ src/main/java/me/pumpkinbear111/valentinesday/blocks/empty_vase.java
@@ -12,7 +12,6 @@ import net.minecraft.util.math.shapes.ISelectionContext;
 import net.minecraft.util.math.shapes.VoxelShape;
 import net.minecraft.util.math.shapes.VoxelShapes;
 import net.minecraft.world.IBlockReader;
-import net.minecraftforge.common.ToolType;
 
 import java.util.stream.Stream;
 
@@ -34,19 +33,7 @@ public class empty_vase extends Block {
     }
 
     private static final VoxelShape SHAPE_N = Stream.of(
-            Block.makeCuboidShape(5, 4, 7, 6, 8, 9),
-            Block.makeCuboidShape(7, 4, 10, 9, 8, 11),
-            Block.makeCuboidShape(7, 4, 5, 9, 8, 6),
-            Block.makeCuboidShape(10, 4, 7, 11, 8, 9),
-            Block.makeCuboidShape(9, 3, 9, 10, 8, 10),
-            Block.makeCuboidShape(9, 3, 6, 10, 8, 7),
-            Block.makeCuboidShape(6, 3, 6, 7, 8, 7),
-            Block.makeCuboidShape(6, 3, 9, 7, 8, 10),
-            Block.makeCuboidShape(7, 1, 6, 9, 4, 7),
-            Block.makeCuboidShape(7, 1, 9, 9, 4, 10),
-            Block.makeCuboidShape(9, 1, 7, 10, 4, 9),
-            Block.makeCuboidShape(6, 1, 7, 7, 4, 9),
-            Block.makeCuboidShape(7, 0, 7, 9, 1, 9)
+            Block.makeCuboidShape(4, 0, 4, 12, 9, 12)
     ).reduce((v1, v2) -> {return VoxelShapes.combineAndSimplify(v1, v2, IBooleanFunction.OR);}).get();
 
 }
