946236554326868cdd7fee092053b5249253b88f
BuildTools <unconfigured@null.spigotmc.org>
2021-01-28 14:49:33 -0800

    Added logo.png for a mod logo, Updated balloon block to face both directions, north/south and east/west. Also updated mods.toml and chose a license (GNU General Public License v3.0)

--- src/main/java/me/pumpkinbear111/valentinesday/blocks/balloon.java
+++ src/main/java/me/pumpkinbear111/valentinesday/blocks/balloon.java
@@ -2,36 +2,32 @@ package me.pumpkinbear111.valentinesday.blocks;
 
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
-import net.minecraft.block.HorizontalBlock;
 import net.minecraft.block.SoundType;
 import net.minecraft.block.material.Material;
-import net.minecraft.state.DirectionProperty;
+import net.minecraft.util.Direction;
 import net.minecraft.util.math.BlockPos;
-import net.minecraft.util.math.shapes.IBooleanFunction;
 import net.minecraft.util.math.shapes.ISelectionContext;
 import net.minecraft.util.math.shapes.VoxelShape;
-import net.minecraft.util.math.shapes.VoxelShapes;
 import net.minecraft.world.IBlockReader;
 
-import java.util.stream.Stream;
+import java.util.HashMap;
+import java.util.Map;
 
-public class balloon extends Block {
+public class balloon extends BaseHorizontalBlock {
 
-    public static final DirectionProperty FACING = HorizontalBlock.HORIZONTAL_FACING;
+    public static final VoxelShape BALLOON_SHAPE = Block.makeCuboidShape(1, 0, 4, 15, 14, 12);
 
     public balloon() {
         super(Block.Properties.create(Material.WOOL)
                 .hardnessAndResistance(.8f, .8f)
                 .sound(SoundType.CLOTH)
                 .notSolid());
+        runCalculation(BALLOON_SHAPE);
     }
 
     @Override
     public VoxelShape getShape(BlockState state, IBlockReader worldIn, BlockPos pos, ISelectionContext context) {
-        return SHAPE_N;
+        return SHAPES.get(state.get(HORIZONTAL_FACING));
     }
 
-    private static final VoxelShape SHAPE_N = Stream.of(
-            Block.makeCuboidShape(1, 0, 4, 15, 14, 12)
-    ).reduce((v1, v2) -> {return VoxelShapes.combineAndSimplify(v1, v2, IBooleanFunction.OR);}).get();
 }
